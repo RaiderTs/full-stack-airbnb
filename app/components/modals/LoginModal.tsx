c7810ba1dbdb6ea68ede0ce5974eefd9e33ea5b5
AlexTs <a.tsotsko@pinta.pro>
2023-05-28 21:29:04 +0300

    feat: adds login modal

---
+++ app/components/modals/LoginModal.tsx
@@ -0,0 +1,125 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { FC } from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal ';
+
+import Modal from '@/app/components/modals/Modal';
+import Heading from '@/app/components/Heading';
+import Input from '@/app/components/inputs/Input';
+import Button from '@/app/components/Button';
+
+interface LoginModalProps {}
+
+const LoginModal: FC<LoginModalProps> = ({}) => {
+  const router = useRouter();
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    signIn('credentials', { ...data, redirect: false }).then((callback) => {
+      setIsLoading(false);
+
+      if (callback?.ok) {
+        toast.success('Logged in');
+        router.refresh();
+        loginModal.onClose();
+      }
+
+      if (callback?.error) {
+        toast.error(callback.error);
+      }
+    });
+  };
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading title='Welcome back' subtitle='Login to your account' />
+      <Input
+        id={'email'}
+        register={register}
+        label={'Email'}
+        disabled={isLoading}
+        errors={errors}
+        required
+      />
+      <Input
+        id={'password'}
+        type={'password'}
+        register={register}
+        label={'Password'}
+        disabled={isLoading}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button
+        outline
+        label={'Continue with Google'}
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label={'Continue with Githab'}
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className='text-neutral-500 text-center mt-4 font-light'>
+        <div className='flex flex-row justify-center gap-2 text-center'>
+          <div>
+            <div>Already have an account?</div>
+          </div>
+          <div
+            className='text-neutral-800 cursor-pointer hover:underline'
+            onClick={registerModal.onClose}
+          >
+            <div>Log in</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title={'Login'}
+      actionLabel={'Continue'}
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default LoginModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import ClientOnly from '@/app/components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from '@/app/components/modals/LoginModal';
 import Navbar from '@/app/components/navbar/Navbar';
 import { Nunito } from 'next/font/google';
-
 import './globals.css';
 import ToasterProvider from '@/app/providers/ToasterProvider';
 
@@ -25,6 +25,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
