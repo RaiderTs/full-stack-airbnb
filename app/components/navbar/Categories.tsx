35cbebd7cfd64732103810162ffa5d60721a9193
AlexTs <a.tsotsko@pinta.pro>
2023-06-12 00:26:46 +0300

    feat: adds Cetegories component with useSearch

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -2,10 +2,42 @@
 
 import Container from '@/app/components/Container';
 
+import { TbBeach } from 'react-icons/tb';
+import { GiWindmill } from 'react-icons/gi';
+import { MdOutlineVilla } from 'react-icons/md';
+import CategoryBox from '@/app/components/CategoryBox';
+
+export const categories = [
+  {
+    label: 'Beach',
+    icon: TbBeach,
+    description: 'This property is close to the beach!',
+  },
+  {
+    label: 'Windmills',
+    icon: GiWindmill,
+    description: 'This property has windmolls!',
+  },
+  {
+    label: 'Modern',
+    icon: MdOutlineVilla,
+    description: 'This property is modern!',
+  },
+];
+
 const Categories = () => {
   return (
     <Container>
-      <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'></div>
+      <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
